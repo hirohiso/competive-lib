1cd867b8209b93a5604bbe9f2edd643132908e2d
Irie HIronori <irie.h126@gmail.com>
2019-09-16 14:01:16 +0900

    DFSをちょっと修正

--- competive-lib/src/main/java/jp/hirohiso/competive/util/graph/Dfs.java
+++ competive-lib/src/main/java/jp/hirohiso/competive/util/graph/Dfs.java
@@ -14,7 +14,7 @@ public class Dfs {
     }
 
     //深さ優先探索
-    public static void dfs(Node root){
+    public static void dfs(Node root) {
         //探索済み
         Set<Node> visited = new HashSet<>();
         //スタック
@@ -25,22 +25,41 @@ public class Dfs {
         stack.addLast(root);
 
         Node target;
-        while((target = stack.pollLast()) != null){
-            List<Node> nextNodes = target.getLinkedNodes();
-            for(Node n :nextNodes){
-                if(!visited.contains(n)){
-                    //訪問済みでない
-                    visited.add(n);
-                    stack.addLast(n);
+        while ((target = stack.pollLast()) != null) {
+            //隣接ノードを取得
+            List<Node> temp = target.getLinkedNodes();
+
+            //未訪問の隣接ノードを取得
+            List<Node> nextNodes = new LinkedList<>();
+            for (Node n : temp) {
+                if (!visited.contains(n)) {
+                    //未訪問だけ積める
+                    nextNodes.add(n);
                 }
+            }
+            //nextNodesが空の場合は、葉
+
+
+            for (Node n : nextNodes) {
+                //未訪問ノードを全てスタックに積む
+                visited.add(n);
+                stack.addLast(n);
+                //のちのち構築した木に親子の関係をしたい場合
+                //ここで子に対して、親の参照を貼っとく
             }
         }
 
     }
-    public static class Node{
+
+    public static class Node {
+        //隣接ノード
         private List<Node> linkedNode = new LinkedList<>();
 
-        public List<Node> getLinkedNodes(){
+        public void addLinkedNode(Node e) {
+            linkedNode.add(e);
+        }
+
+        public List<Node> getLinkedNodes() {
             return linkedNode;
         }
 
