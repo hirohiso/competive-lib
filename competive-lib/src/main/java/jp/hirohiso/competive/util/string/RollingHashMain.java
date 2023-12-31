9f143b4915780b812830cffeab936b4a39d67abe
hiro <irie.h126@gmail.com>
2023-12-31 23:01:05 +0900

    微修正

--- competive-lib/src/main/java/jp/hirohiso/competive/util/string/RollingHashMain.java
+++ competive-lib/src/main/java/jp/hirohiso/competive/util/string/RollingHashMain.java
@@ -1,43 +1,37 @@
 package jp.hirohiso.competive.util.string;
 
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
-import java.util.function.Consumer;
-import java.util.function.LongConsumer;
 
 public class RollingHashMain {
 
     public static void main(String[] args) {
         var rh = new RollingHash();
         var hash = rh.get("abc");
-        for(long l : rh.iterator("abcdefabcgdegrwetjrsabc",3)){
+        for (long l : rh.iterator("abcdefabcgdegrwetjrsabc", 3)) {
             System.out.println(l);
         }
 
     }
 
-    public static class RollingHash{
+    public static class RollingHash {
         private long b = 3491;
         private long m = 999999937;
         private char zero = 'a';
 
 
         //strのハッシュ値を返す
-        public long get(String str){
+        public long get(String str) {
             var size = str.length();
             var hash = 0l;
             for (int i = 0; i < size; i++) {
-                hash *= b;
-                hash %= m;
-                hash += (str.charAt(i) - zero);
-                hash %= m;
+                hash = shift(hash, (str.charAt(i) - zero));
             }
             return hash;
         }
 
         //
-        public Iterable<Long> iterator(String str, int size){
-            var result = new Iterable<Long>(){
+        public Iterable<Long> iterator(String str, int size) {
+            var result = new Iterable<Long>() {
                 private String str;
                 private int size;
                 private long b;
@@ -46,7 +40,7 @@ public class RollingHashMain {
                 private int now = -1;
                 private long hash = 0;
 
-                public Iterable<Long> init(String str,int size,long b,long m){
+                public Iterable<Long> init(String str, int size, long b, long m) {
                     this.str = str;
                     this.size = size;
                     this.b = b;
@@ -63,36 +57,38 @@ public class RollingHashMain {
                     return new Iterator<>() {
                         @Override
                         public boolean hasNext() {
-                            return now + size  < str.length();
+                            return now + size < str.length();
                         }
 
                         @Override
                         public Long next() {
                             now++;
-                            if(now == 0){
+                            if (now == 0) {
                                 for (int i = 0; i < size; i++) {
-                                    hash *= b;
-                                    hash %= m;
-                                    hash += (str.charAt(i) - zero);
-                                    hash %= m;
+                                    hash = shift(hash, (str.charAt(i) - zero));
                                 }
                                 return hash;
                             }
-                            var h=(long)(str.charAt(now -1) - zero);
+                            var h = (long) (str.charAt(now - 1) - zero);
                             h *= bsize;
                             h %= m;
-                            hash -=h;
-                            hash *= b;
-                            hash %= m;
-                            hash += (str.charAt(now + (size -1)) - zero);
-                            hash %= m;
+                            hash -= h;
+                            hash = shift(hash, (str.charAt(now + (size - 1)) - zero));
                             return hash;
                         }
                     };
                 }
 
-            }.init(str,size,b,m);
+            }.init(str, size, b, m);
             return result;
+        }
+
+        private long shift(long h, long c) {
+            h *= b;
+            h %= m;
+            h += c;
+            h %= m;
+            return h;
         }
 
     }
