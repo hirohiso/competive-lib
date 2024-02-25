47685fa14462b9ce28f37530c797e22cd6f47545
hiro <irie.h126@gmail.com>
2024-02-25 10:42:51 +0900

    素因数分解のライブラリ追加

--- competive-lib/src/main/java/jp/hirohiso/competive/util/math/Prime.java
+++ competive-lib/src/main/java/jp/hirohiso/competive/util/math/Prime.java
@@ -1,6 +1,7 @@
 package jp.hirohiso.competive.util.math;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -8,7 +9,8 @@ public class Prime {
 
     public static void main(String[] args) {
         boolean[] primeTable = prime(10_000_000);
-        Map<Long, Integer> map = factorization(100_000_007, primeTable);
+        var spf = spf(100_000_007);
+        Map<Long, Integer> map = factorization(100_000_007, spf);
         map.forEach((k, i) -> {
             System.out.println(k + "^" + i);
         });
@@ -32,7 +34,9 @@ public class Prime {
         return result;
     }
 
+
     //素因数分解
+    //ルート n???
     public static Map<Long, Integer> factorization(long n, boolean[] prime) {
         Map<Long, Integer> map = new TreeMap<>();
         long temp = n;
@@ -51,6 +55,36 @@ public class Prime {
         }
         if (temp != 1) {
             map.put(temp, 1);
+        }
+        return map;
+    }
+
+    //SPF利用　素因数分解
+
+    //素因数分解　前処理N log log N
+    public static int[] spf(int n) {
+        int[] spf = new int[n + 1];
+        Arrays.fill(spf, 0);
+        for (int i = 0; i < spf.length; i++) {
+            spf[i] = i;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if(spf[i] == i){
+                for (int j = i * i; j <= n; j+=i) {
+                    if(spf[j] == j){
+                        spf[j] = i;
+                    }
+                }
+            }
+        }
+        return spf;
+    }
+    //素因数分解 log n
+    public static Map<Long, Integer> factorization(int n,int[] spf) {
+        Map<Long, Integer> map = new HashMap<>();
+        while (n != 1){
+            map.merge((long)spf[n], 1 , Integer::sum);
+            n /= spf[n];
         }
         return map;
     }
