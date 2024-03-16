24963b02044028000eaa8a1c9712131371ac280f
hiro <irie.h126@gmail.com>
2024-03-17 01:07:55 +0900

    最長部分増加列の修正 + 増加列復元の実装

--- competive-lib/src/main/java/jp/hirohiso/competive/util/sequence/Sequence.java
+++ competive-lib/src/main/java/jp/hirohiso/competive/util/sequence/Sequence.java
@@ -1,25 +1,76 @@
 package jp.hirohiso.competive.util.sequence;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Sequence {
 
-    //LSI 最長部分増加列
-    public static int lsi(int[] array){
-        int N = array.length;//数値の個数
-
-        int[] dp = new int[N];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0] = array[0];
-        for (int i = 1; i < N; i++) {
-            int a = array[i];//n番目の数
-            if(dp[i-1] < a){
-                dp[i] = a;
-                continue;
-            }
-            int point = (-(Arrays.binarySearch(dp,a)))-1;//挿入ポイント
-            dp[point] = a;
+    public static void main(String[] args) {
+        var arr = new int[]{3, 2, 4, 1, 3, 5, 2, 100};
+        var list = LongestIncreasingSubsequence.lis(arr);
+        System.out.println(list.size());
+        for (Integer i : list){
+            System.out.print(i + " ");
         }
-        return (-Arrays.binarySearch(dp,Integer.MAX_VALUE-1));
+        System.out.println();
+    }
+
+
+    public static class LongestIncreasingSubsequence {
+        //LIS 最長部分増加列
+        public static List<Integer> lis(int[] array) {
+            int N = array.length;//数値の個数
+
+            var dp = new int[N + 1];
+            var dpi = new int[N + 1];//dpが示す要素のインデックス
+            var table = new int[N];//自分より小さな要素。先頭の場合は-1
+            Arrays.fill(table, -1);
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            Arrays.fill(dpi, -1);
+            dp[0] = array[0];
+            dpi[0] = 0;
+            var n = 1; //現在のdpの末尾
+            for (int i = 1; i < N; i++) {
+                var a = array[i];//n番目の数
+                if (dp[n - 1] < a) {
+                    dp[n] = a;
+                    dpi[n] = i;
+                    table[i] = dpi[n - 1];
+                    n++;
+                    continue;
+                }
+                var point = lb(dp, a);//挿入ポイント
+                dp[point] = a;
+                dpi[point] = i;
+                if (point == 0) {
+                    table[i] = -1;
+                } else {
+                    table[i] = dpi[point - 1];
+                }
+            }
+            var tail = dpi[lb(dp, Integer.MAX_VALUE) - 1];
+            var list = new LinkedList<Integer>();
+            while (tail != -1) {
+                list.addFirst(array[tail]);
+                tail = table[tail];
+            }
+            return list;
+        }
+
+        public static final int lb(final int[] arr, final long value) {
+            int low = 0;
+            int high = arr.length;
+            int mid;
+            while (low < high) {
+                mid = ((high - low) >>> 1) + low;    //(low + high) / 2 (オーバーフロー対策)
+                if (arr[mid] < value) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            return low;
+        }
     }
 }
