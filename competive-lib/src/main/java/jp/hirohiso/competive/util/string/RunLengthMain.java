49ac993d0f43bc87e4716174fd866d61b28cb8dc
hiro <irie.h126@gmail.com>
2024-03-16 21:10:52 +0900

    プリミティブ型に変更、TLEになる恐れがあるのでArrayListに変更

--- competive-lib/src/main/java/jp/hirohiso/competive/util/string/RunLengthMain.java
+++ competive-lib/src/main/java/jp/hirohiso/competive/util/string/RunLengthMain.java
@@ -1,19 +1,20 @@
 package jp.hirohiso.competive.util.string;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class RunLengthMain {
     public static void main(String[] args) {
         System.out.println(runLengthEncoding("aaaBBcccc"));
     }
-    public static LinkedList<TPair<Character, Integer>> runLengthEncoding(String str) {
-        var result = new LinkedList<TPair<Character, Integer>>();
+    public static ArrayList<CharIntPair> runLengthEncoding(String str) {
+        var result = new ArrayList<CharIntPair>();
         char c = 0;
         int num = 0;
         for (int i = 0; i < str.length(); i++) {
             if (c != str.charAt(i)) {
                 if (i > 0) {
-                    result.addLast(new TPair<>(c, num));
+                    result.add(new CharIntPair(c, num));
                 }
                 c = str.charAt(i);
                 num = 1;
@@ -21,10 +22,10 @@ public class RunLengthMain {
                 num++;
             }
         }
-        result.addLast(new TPair<>(c, num));
+        result.add(new CharIntPair(c, num));
         return result;
     }
 
-    record TPair<S, T>(S a, T b) {
+    record CharIntPair(char a, int b) {
     }
 }
