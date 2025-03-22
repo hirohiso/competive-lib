424628da35c34d6ddb3b6825422a00837b82e968
hiro <irie.h126@gmail.com>
2025-03-22 14:41:42 +0900

    組み合わせライブラリをアップデート

--- competive-lib/src/main/java/jp/hirohiso/competive/util/math/Combination.java
+++ competive-lib/src/main/java/jp/hirohiso/competive/util/math/Combination.java
@@ -3,35 +3,40 @@ package jp.hirohiso.competive.util.math;
 public class Combination {
 
     public static void main(String[] args) {
-        COMinit();
-        System.out.println(inv[2]);
+        var com = new CombinationHelper(1000000, 1000000007);
 
-        System.out.println(COM(8, 3));
+
+        System.out.println(com.cal(8, 3));
     }
 
-    static int MAX = 4000;
-    static int MOD = 1000_000_007;
-    static long fac[] = new long[MAX];
-    static long finv[] = new long[MAX];
-    static long inv[] = new long[MAX];
+    static class CombinationHelper{
+        final int MAX;
+        final int MOD;
+        final long[] fac = new long[MAX];
+        final long[] finv = new long[MAX];
+        final long[] inv = new long[MAX];
 
-    public static void COMinit() {
-        fac[0] = fac[1] = 1;
-        finv[0] = finv[1] = 1;
-        inv[1] = 1;
-        for (int i = 2; i < MAX; i++) {
-            fac[i] = fac[i - 1] * i % MOD;
-            inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-            finv[i] = finv[i - 1] * inv[i] % MOD;
+        public CombinationHelper(int max,int mod) {
+            MOD = mod;
+            MAX = max;
+            fac[0] = fac[1] = 1;
+            finv[0] = finv[1] = 1;
+            inv[1] = 1;
+            for (int i = 2; i < MAX; i++) {
+                fac[i] = fac[i - 1] * i % MOD;
+                inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+                finv[i] = finv[i - 1] * inv[i] % MOD;
+            }
+        }
+
+        // 二項係数計算
+        public long cal(int n, int k) {
+            if (n < k)
+                return 0;
+            if (n < 0 || k < 0)
+                return 0;
+            return  (fac[n] * (finv[k] * finv[n - k] % MOD) % MOD);
         }
     }
 
-    // 二項係数計算
-    public static int COM(int n, int k) {
-        if (n < k)
-            return 0;
-        if (n < 0 || k < 0)
-            return 0;
-        return (int) (fac[n] * (finv[k] * finv[n - k] % MOD) % MOD);
-    }
 }
