abc8326b4a22b3c73bd5880cb6ed71bfc186fc6e
hiro <irie.h126@gmail.com>
2026-01-25 00:35:56 +0900

    偏角ソート

--- competive-lib/src/main/java/jp/hirohiso/competive/util/math/ArgumentSort.java
+++ competive-lib/src/main/java/jp/hirohiso/competive/util/math/ArgumentSort.java
@@ -3,11 +3,14 @@ package jp.hirohiso.competive.util.math;
 public class ArgumentSort {
     public static void main(String[] args) {
         Point[] points = {
-            new Point(1, 2),
-            new Point(2, 1),
-            new Point(3, 3),
-            new Point(0, 4),
-            new Point(4, 0)
+                new Point(-1, 0),
+                new Point(-1, -1),
+                new Point(0, -1),
+                new Point(1, 1),
+                new Point(-1, 1),
+                new Point(1, -1),
+                new Point(1, 0),
+                new Point(0, 1),
         };
 
         // 偏角順にソート
@@ -23,6 +26,11 @@ public class ArgumentSort {
 record Point(int x, int y) implements Comparable<Point> {
     @Override
     public int compareTo(Point o) {
+        boolean h0 = (this.y < 0) || (this.y == 0 && this.x < 0); // (y0, x0) < (0, 0)
+        boolean h1 = (o.y < 0) || (o.y == 0 && o.x < 0); // (y1, x1) < (0, 0)
+
+        int c = Boolean.compare(h0, h1); // false < true
+        if (c != 0) return c;
         //偏角順で比較
         return Integer.compare(y * o.x, x * o.y);
     }
